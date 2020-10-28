0d2ce01fa30bd61e7f23408dc1ecedefa53044da
Brenden Gonzalez <brendenrichardgonzalez@gmail.com>
2020-10-28 17:58:41 -0500

    Update gapps.sh

--- gapps.sh
+++ gapps.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+#!/bin/bash
+echo "Downloading manifest for OpenGapps"
+echo "This script only needs to be ran onetime! Unless you are downloading lineageOS source code again!"
+wget https://raw.githubusercontent.com/gonzalezb/gsi_gapps_manifest/main/opengapps.xml -P .repo/manifests
+ls .repo/manifests
+echo "Done now run: bash treble_build_los/buildbot_treble.sh"
+echo ""
