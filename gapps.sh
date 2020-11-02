ea45f3427b849016d5e84957ea3be1d048630b08
Brenden Gonzalez <brendenrichardgonzalez@gmail.com>
2020-11-02 11:57:10 -0600

    Properly import manifest file

--- gapps.sh
+++ gapps.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 echo "Downloading manifest for OpenGapps"
 echo "This script only needs to be ran onetime! Unless you are downloading lineageOS source code again!"
-wget https://raw.githubusercontent.com/gonzalezb/gsi_gapps_manifest/main/opengapps.xml -P .repo/manifests
+mkdir .repo/local_manifests
+wget https://raw.githubusercontent.com/gonzalezb/gsi_gapps_manifest/main/opengapps.xml -P .repo/local_manifests
 ls .repo/local_manifests
 echo "Done now run: bash treble_build_los/buildbot_treble.sh"
 echo ""
