5ee89dc96e8938b91a16497beabdfc4cc3b912d4
Brenden Gonzalez <brendenrichardgonzalez@gmail.com>
2020-11-02 14:29:17 -0600

    Gapps script was merged with buildbot script

--- gapps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#!/bin/bash
-echo "Downloading manifest for OpenGapps"
-echo "This script only needs to be ran onetime! Unless you are downloading lineageOS source code again!"
-mkdir .repo/local_manifests
-wget https://raw.githubusercontent.com/gonzalezb/gsi_gapps_manifest/main/opengapps.xml -P .repo/local_manifests
-ls .repo/local_manifests
-echo "Done now run: bash treble_build_los/buildbot_treble.sh"
-echo ""
